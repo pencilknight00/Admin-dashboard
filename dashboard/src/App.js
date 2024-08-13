37a2fcbe40012f99b774272b7037f4bf49e2befd
Do Van Viet <pencilknight00@gmail.com>
2024-08-13 10:41:10 +0700

    added theme.js
    theme.js create theme context for the app (dark or light mode)

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,10 +1,22 @@
-
+import { colorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  
   return (
-    <div className="app">
-      <h1>Dashboard</h1>
-    </div>
+    <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider>
+        <CssBaseline>
+          <div className="app">
+            <main className="content">
+              <Topbar/>
+            </main>
+          </div>
+        </CssBaseline>
+      </ThemeProvider>
+    </colorModeContext.Provider>
   );
 }
 
--- dashboard/src/scenes/global/Topbar.jsx
+++ dashboard/src/scenes/global/Topbar.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import { Box, IconButton, useTheme } from '@mui/material';
+import { useContext } from 'react';
+import { colorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const Topbar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
     return (
         <div>
             <h1>Topbar</h1>
